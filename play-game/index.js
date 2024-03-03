1ea5855a5dbbd40c98349b0214574b11be023b31
Vitor Menoli <vitoragario@gmail.com>
2024-03-03 18:57:17 -0300

    Alterando arquivos

--- play-game/index.js
+++ play-game/index.js
@@ -7,8 +7,7 @@ function toggleColor(element, toggleClass) {
     }, 500)
 }
 
-function buttonClick() {
-    console.log(correctNumber)
+function mainButton() {
     const buttonTry = document.querySelector('.buttonTry')
     
     buttonTry.addEventListener('click', (ev) => {
@@ -18,7 +17,8 @@ function buttonClick() {
             return
         }
 
-        const tryNumber = document.querySelector('#tryNumber').value
+        const inputNuber = document.querySelector('#tryNumber')
+        const tryNumber = inputNuber.value
         const chutes = document.querySelector('#numberChutes')
         const dica = document.querySelector('#dicaText')
         const ultimoChute = document.querySelector('#ultimoChute')
@@ -30,12 +30,16 @@ function buttonClick() {
         if (!tryNumber) {
             dica.textContent = 'Chute um número!'
             toggleColor(liDica, 'dica')
+
+            inputNuber.value = ''
+            inputNuber.focus()
         }
         else if (tryNumber < 1 || tryNumber > 1000) {
             dica.textContent = 'Chute um número de 1 a 1000!'
             toggleColor(liDica, 'dica')
 
-            document.querySelector('#tryNumber').value = ''
+            inputNuber.value = ''
+            inputNuber.focus()
         }
         else if (tryNumber > correctNumber) {
             dica.textContent = 'Chute um número mais baixo!'
@@ -50,7 +54,8 @@ function buttonClick() {
             ultimoChute.textContent = tryNumber
             toggleColor(liUltimoChute, 'ultimoChute')
 
-            document.querySelector('#tryNumber').value = ''
+            inputNuber.value = ''
+            inputNuber.focus()
         }
         else if (tryNumber < correctNumber) {
             dica.textContent = 'Chute um número mais alto!'
@@ -65,7 +70,8 @@ function buttonClick() {
             ultimoChute.textContent = tryNumber
             toggleColor(liUltimoChute, 'ultimoChute')
 
-            document.querySelector('#tryNumber').value = ''
+            inputNuber.value = ''
+            inputNuber.focus()
         }
         else if (tryNumber == correctNumber) {
 
@@ -83,8 +89,8 @@ function buttonClick() {
 
             title.textContent = `Parabéns, você acertou o número: ${correctNumber}`
 
-            document.querySelector('#tryNumber').classList.add('disabledInput')
-            document.querySelector('#tryNumber').disabled = true
+            inputNuber.classList.add('disabledInput')
+            inputNuber.disabled = true
 
             const ul = document.querySelector('ul')
 
@@ -111,6 +117,127 @@ function buttonClick() {
             }, 500)  
         }
     })
+}
+
+function mainEnter() {
+    const buttonTry = document.querySelector('.buttonTry')
+    
+    document.addEventListener('keydown', (ev) => {
+        
+        if (buttonTry.textContent === 'Recomeçar!') {
+            location.replace('../welcome/index.html')
+            return
+        }
+
+        if (ev.key !== 'Enter') {
+            return
+        }
+
+        const inputNuber = document.querySelector('#tryNumber')
+        const tryNumber = inputNuber.value
+        const chutes = document.querySelector('#numberChutes')
+        const dica = document.querySelector('#dicaText')
+        const ultimoChute = document.querySelector('#ultimoChute')
+
+        const liChutes = document.querySelector('.liChutes')
+        const liDica = document.querySelector('.liDica')
+        const liUltimoChute = document.querySelector('.liUltimoChute')
+
+        if (!tryNumber) {
+            dica.textContent = 'Chute um número!'
+            toggleColor(liDica, 'dica')
+
+            inputNuber.value = ''
+            inputNuber.focus()
+        }
+        else if (tryNumber < 1 || tryNumber > 1000) {
+            dica.textContent = 'Chute um número de 1 a 1000!'
+            toggleColor(liDica, 'dica')
+
+            inputNuber.value = ''
+            inputNuber.focus()
+        }
+        else if (tryNumber > correctNumber) {
+            dica.textContent = 'Chute um número mais baixo!'
+            toggleColor(liDica, 'dica')
+
+            let numero = chutes.textContent
+            numero++
+
+            chutes.textContent = numero
+            toggleColor(liChutes, 'chutes')
+
+            ultimoChute.textContent = tryNumber
+            toggleColor(liUltimoChute, 'ultimoChute')
+
+            inputNuber.value = ''
+            inputNuber.focus()
+        }
+        else if (tryNumber < correctNumber) {
+            dica.textContent = 'Chute um número mais alto!'
+            toggleColor(liDica, 'dica')
+
+            let numero = chutes.textContent
+            numero++
+
+            chutes.textContent = numero
+            toggleColor(liChutes, 'chutes')
+
+            ultimoChute.textContent = tryNumber
+            toggleColor(liUltimoChute, 'ultimoChute')
+
+            inputNuber.value = ''
+            inputNuber.focus()
+        }
+        else if (tryNumber == correctNumber) {
+
+            const buttonDesistir = document.querySelector('.desistir')
+            buttonDesistir.style.display = 'none'
+
+
+            let numero = chutes.textContent
+            numero++
+
+            chutes.textContent = numero
+            toggleColor(liChutes, 'chutes')
+
+            const title = document.querySelector('.title')
+
+            title.textContent = `Parabéns, você acertou o número: ${correctNumber}`
+
+            inputNuber.classList.add('disabledInput')
+            inputNuber.disabled = true
+
+            const ul = document.querySelector('ul')
+
+            ul.removeChild(liDica)
+            ul.removeChild(liUltimoChute)
+
+            buttonTry.textContent = 'Recomeçar!'
+
+            setTimeout(() => {
+                let recordeAtual = Object(JSON.parse(localStorage.getItem('recorde'))).number
+
+                if (!recordeAtual || parseInt(recordeAtual) > parseInt(chutes.textContent)) {
+    
+                    const saveRecord = confirm('Você deseja salvar seu recorde?')
+    
+                    if (saveRecord) {
+                        const name = prompt('Qual o seu nome?')
+        
+                        alert('Recorde salvo!')
+    
+                        localStorage.setItem('recorde', `{ "number": "${chutes.textContent}", "user": "${name}" }`)
+                    }
+                }
+            }, 500)  
+        }
+    })
+}
+
+function buttonClick() {
+    mainButton()
+    mainEnter()
 }
 
 function desistir() {
